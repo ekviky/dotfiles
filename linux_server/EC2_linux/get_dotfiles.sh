437bcbfa063de984631511fa551bd5052561b007
ekviky <ekviky@users.noreply.github.com>
2018-11-13 11:32:50 -0500

    update bootstrap script

--- linux_server/EC2_linux/get_dotfiles.sh
+++ linux_server/EC2_linux/get_dotfiles.sh
@@ -5,21 +5,12 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # vim
 ln -s ${BASEDIR}/vimrc ~/.vimrc
-ln -s ${BASEDIR}/vim/ ~/.vim
 
 # bash
 ln -s ${BASEDIR}/bashrc ~/.bashrc
 ln -s ${BASEDIR}/bash_profile ~/.bash_profile
 
-# git
-ln -s ${BASEDIR}/gitignore ~/.gitignore
-ln -s ${BASEDIR}/gitconfig ~/.gitconfig
-
 # R
 ln -s ${BASEDIR}/Renviron ~/.Renviron
 ln -s ${BASEDIR}/Rprofile ~/.Rprofile
 
-# other
-ln -s ${BASEDIR}/cups/ ~/.cups
-ln -s ${BASEDIR}/domino.vmoptions ~/.domino.vmoptions
-ln -s ${BASEDIR}/aws/ ~/.aws
